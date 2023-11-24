93028aebf0ac00d779316aa8f54551f1e9f8f7ae
musabd96 <nageyejr96@gmail.com>
2023-11-24 14:33:35 +0100

    Test: Refactor AddDogCommandHandler to handle empty dog names
    - Update Handle method to return null if the dog name is empty
    - Adjust test cases to reflect the new behavior

---
+++ Test/DogTests/CommandTest/AddDogTests.cs
@@ -0,0 +1,55 @@
+﻿
+
+using Application.Commands.Dogs;
+using Application.Dtos;
+using Infrastructure.Database;
+
+namespace Test.DogTests.CommandTest
+{
+    [TestFixture]
+    public class AddDogCommandHandlerTests
+    {
+        private AddDogCommandHandler _handler;
+        private MockDatabase _mockDatabase;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Initialize the handler and mock database before each test
+            _mockDatabase = new MockDatabase();
+            _handler = new Application.Commands.Dogs.AddDogCommandHandler(_mockDatabase);
+        }
+
+        [Test]
+        public async Task Handle_ValidCommand_AddNewDog()
+        {
+            // Arrange
+            var command = new AddDogCommand(new DogDto { Name = "NewDog" });
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            var newDogInDatabase = _mockDatabase.Dogs.FirstOrDefault(dog => dog.Name == "NewDog");
+
+            Assert.IsNotNull(newDogInDatabase);
+            Assert.That(newDogInDatabase.Name, Is.EqualTo("NewDog"));
+        }
+
+        [Test]
+        public async Task Handle_InValidCommand_EmptyDogName()
+        {
+            // Arrange
+            var command = new AddDogCommand(new DogDto { Name = "" });
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+
+
+    }
+}
