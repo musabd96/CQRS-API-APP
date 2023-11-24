d4fb1cbf31e0a7ef8fc372ad342e34f245f760e4
musabd96 <nageyejr96@gmail.com>
2023-11-24 23:11:13 +0100

    Feature: Add new Animal in Domain project

---
+++ Domain/Models/Animal/Cat.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Domain.Models.Animal
+{
+    internal class Cat : AnimalModel
+    {
+        public string Meow()
+        {
+            return "This animal meows";
+        }
+    }
+}
