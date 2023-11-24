fa26d147f9addefd783799b08952514f297dd699
musabd96 <nageyejr96@gmail.com>
2023-11-24 23:07:48 +0100

    Feature: Add new animatl contoller in API project

---
+++ API/Controllers/CatsController/CatsController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.CatsController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatsController : Controller
+    {
+        internal readonly IMediator _mediator;
+        public CatsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
