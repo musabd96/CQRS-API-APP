933dd34f2c301669bd6c5edca3b2a5f4a34cbc2a
musabd96 <nageyejr96@gmail.com>
2023-11-24 22:44:58 +0100

    Test : Add test for handling valid and invalid database in GetAllDogs

--- Infrastructure/Database/MockDatabase.cs
+++ Infrastructure/Database/MockDatabase.cs
@@ -2,7 +2,7 @@
 
 namespace Infrastructure.Database
 {
-    public class MockDatabase
+    public class MockDatabase : ICloneable
     {
         public List<Dog> Dogs
         {
@@ -10,13 +10,26 @@ namespace Infrastructure.Database
             set { allDogs = value; }
         }
 
+
         private static List<Dog> allDogs = new()
         {
             new Dog { Id = Guid.NewGuid(), Name = "Björn"},
             new Dog { Id = Guid.NewGuid(), Name = "Patrik"},
             new Dog { Id = Guid.NewGuid(), Name = "Alfred"},
             new Dog { Id = new Guid("12345678-1234-5678-1234-567812345670"), Name = "TestDogForUnitTests"},
+            new Dog { Id = new Guid("12345678-1234-5678-1234-567812345671"), Name = "TestDogForUnitTests"},
             new Dog { Id = new Guid("12345678-1234-5678-1234-567812345678"), Name = "TestDogForUnitTests"}
         };
+
+
+        public object Clone()
+        {
+            MockDatabase clone = new MockDatabase
+            {
+                Dogs = new List<Dog>(Dogs)
+            };
+
+            return clone;
+        }
     }
 }
