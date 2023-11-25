83ad92bf5d8022744e30b17e7d9e8257b12dd29f
musabd96 <mustafa.abdulle@student.nbi-handelsakademin.se>
2023-11-25 11:10:07 +0100

    Feature: Add update cat in mockdatabase

---
+++ Application/Commands/Cats/UpdateCat/UpdateCatByIdCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Models;
+using Infrastructure.Database;
+using MediatR;
+
+namespace Application.Commands.Cats.UpdateCat
+{
+    public class UpdateCatByIdCommandHandler : IRequestHandler<UpdateCatByIdCommand, Cat>
+    {
+        private readonly MockDatabase _mockDatabase;
+
+        public UpdateCatByIdCommandHandler(MockDatabase mockDatabase)
+        {
+            _mockDatabase = mockDatabase;
+        }
+
+        public Task<Cat> Handle(UpdateCatByIdCommand request, CancellationToken cancellationToken)
+        {
+            Cat catToUpdate = _mockDatabase.Cats.FirstOrDefault(cat => cat.Id == request.Id)!;
+
+            if (catToUpdate != null)
+            {
+                catToUpdate.Name = request.UpdatedCat.Name;
+                return Task.FromResult(catToUpdate);
+            }
+
+            return Task.FromResult(catToUpdate);
+        }
+    }
+}
