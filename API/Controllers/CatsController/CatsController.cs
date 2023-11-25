53e6aba6114de78bd908e2eea738c6576c532119
musabd96 <mustafa.abdulle@student.nbi-handelsakademin.se>
2023-11-25 11:37:58 +0100

    Feature: Delete cat by id

--- API/Controllers/CatsController/CatsController.cs
+++ API/Controllers/CatsController/CatsController.cs
@@ -1,5 +1,7 @@
 ﻿using Application.Commands.Cats;
+using Application.Commands.Cats.DeleteCat;
 using Application.Commands.Cats.UpdateCat;
+using Application.Commands.Dogs.DeleteDog;
 using Application.Commands.Dogs.UpdateDog;
 using Application.Dtos;
 using Application.Queries.Cats.GetById;
@@ -43,12 +45,20 @@ namespace API.Controllers.CatsController
             return Ok(await _mediator.Send(new AddCatCommand(newCat)));
         }
 
-        // Update a specific dog
+        // Update a specific cat
         [HttpPut]
         [Route("updateCat/{updatedCatId}")]
         public async Task<IActionResult> UpdateCat([FromBody] CatDto updatedCat, Guid updatedCatId)
         {
             return Ok(await _mediator.Send(new UpdateCatByIdCommand(updatedCat, updatedCatId)));
+        }
+
+        // Delete a cat by Id
+        [HttpDelete]
+        [Route("deleteCatById/{catId}")]
+        public async Task<IActionResult> DeleteCatById(Guid catId)
+        {
+            return Ok(await _mediator.Send(new DeleteCatByIdCommand(catId)));
         }
     }
 }
