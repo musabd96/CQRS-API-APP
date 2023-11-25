0996ad0659cb5a87836c5064bdbebc5fd40db465
musabd96 <mustafa.abdulle@student.nbi-handelsakademin.se>
2023-11-25 10:12:08 +0100

    Feature: Add Get cat by id

---
+++ Application/Queries/Cats/GetById/GetCatByIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Models;
+using Infrastructure.Database;
+using MediatR;
+
+namespace Application.Queries.Cats.GetById
+{
+    public class GetCatByIdQueryHandler : IRequestHandler<GetCatByIdQuery, Cat>
+    {
+        private readonly MockDatabase _mockDatabase;
+
+        public GetCatByIdQueryHandler(MockDatabase mockDatabase)
+        {
+            _mockDatabase = mockDatabase;
+        }
+
+        public Task<Cat> Handle(GetCatByIdQuery request, CancellationToken cancellationToken)
+        {
+            Cat wantedCat = _mockDatabase.Cats.FirstOrDefault(Cat => Cat.Id == request.Id)!;
+            return Task.FromResult(wantedCat);
+        }
+    }
+}
