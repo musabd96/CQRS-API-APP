db4e0b9e199f84c7995b1dea1037fd563fb6f7c0
musabd96 <113942193+musabd96@users.noreply.github.com>
2023-12-10 11:11:52 +0000

    Feature: Add real Db and nunit test.

--- Application/Queries/Birds/GetById/GetBirdByIdQueryHandler.cs
+++ Application/Queries/Birds/GetById/GetBirdByIdQueryHandler.cs
@@ -6,17 +6,26 @@ namespace Application.Queries.Birds.GetById
 {
     public class GetBirdByIdQueryHandler : IRequestHandler<GetBirdByIdQuery, Bird>
     {
-        private readonly MockDatabase _mockDatabase;
+        private readonly AppDbContext _dbContext;
 
-        public GetBirdByIdQueryHandler(MockDatabase mockDatabase)
+        public GetBirdByIdQueryHandler(AppDbContext dbContext)
         {
-            _mockDatabase = mockDatabase;
+            _dbContext = dbContext;
         }
 
         public Task<Bird> Handle(GetBirdByIdQuery request, CancellationToken cancellationToken)
         {
-            Bird wantedBird = _mockDatabase.Birds.FirstOrDefault(bird => bird.Id == request.Id)!;
+            Bird wantedBird = _dbContext.Birds.FirstOrDefault(bird => bird.Id == request.Id)!;
+
+            if (wantedBird == null)
+            {
+                // Return null if the bird is not found
+                return Task.FromResult<Bird>(null);
+            }
+
             return Task.FromResult(wantedBird);
         }
+
+
     }
 }
