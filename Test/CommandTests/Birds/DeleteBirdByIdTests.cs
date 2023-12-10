a545663977351f118f86f60a3d3b5411f664bbec
musabd96 <nageyejr96@gmail.com>
2023-12-10 15:16:12 +0100

    Feature: Delete bird by id add Db and test unit

--- Test/CommandTests/Birds/DeleteBirdByIdTests.cs
+++ Test/CommandTests/Birds/DeleteBirdByIdTests.cs
@@ -1,5 +1,8 @@
 ﻿using Application.Commands.Birds.DeleteBird;
+using Domain.Models;
 using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Moq;
 
 namespace Test.CommandTests.Birds
 {
@@ -7,21 +10,40 @@ namespace Test.CommandTests.Birds
     public class DeleteBirdByIdTests
     {
         private DeleteBirdByIdCommandHandler _handler;
-        private MockDatabase _mockDatabase;
+        private Mock<AppDbContext> _dbContextMock;
 
         [SetUp]
         public void SetUp()
         {
             // Initialize the original database and create a clone for each test
-            _mockDatabase = new MockDatabase();
-            _handler = new DeleteBirdByIdCommandHandler(_mockDatabase);
+            _dbContextMock = new Mock<AppDbContext>();
+            _handler = new DeleteBirdByIdCommandHandler(_dbContextMock.Object);
+        }
+
+        protected void SetupMockDbContext(List<Bird> birds)
+        {
+            var mockDbSet = new Mock<DbSet<Bird>>();
+            mockDbSet.As<IQueryable<Bird>>().Setup(m => m.Provider).Returns(birds.AsQueryable().Provider);
+            mockDbSet.As<IQueryable<Bird>>().Setup(m => m.Expression).Returns(birds.AsQueryable().Expression);
+            mockDbSet.As<IQueryable<Bird>>().Setup(m => m.ElementType).Returns(birds.AsQueryable().ElementType);
+            mockDbSet.As<IQueryable<Bird>>().Setup(m => m.GetEnumerator()).Returns(birds.GetEnumerator());
+
+            _dbContextMock.Setup(b => b.Birds).Returns(mockDbSet.Object);
         }
 
         [Test]
         public async Task Handle_ValidId_DeletesBird()
         {
             // Arrange
-            var birdId = new Guid("12345678-1234-5678-1234-567812345670");
+            var birdId = new Guid("12345678-1234-5678-1234-567812345678");
+            var bird = new List<Bird>
+            {
+                new Bird
+                {
+                    Id = birdId
+                }
+            };
+            SetupMockDbContext(bird);
 
             var command = new DeleteBirdByIdCommand(birdId);
 
@@ -38,6 +60,8 @@ namespace Test.CommandTests.Birds
         {
             // Arrange
             var invalidBirdId = Guid.NewGuid();
+            var bird = new List<Bird>();
+            SetupMockDbContext(bird);
 
             var command = new DeleteBirdByIdCommand(invalidBirdId);
 
