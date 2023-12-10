2ebc5414a85e3a8c015b9a1a123cd5af6a15b6a7
musabd96 <113942193+musabd96@users.noreply.github.com>
2023-12-10 09:47:17 +0000

    Bug: new class in Test project for TestAsyncEnumerator

--- Test/QueryTests/Birds/GetAllBirdsTests.cs
+++ Test/QueryTests/Birds/GetAllBirdsTests.cs
@@ -69,28 +69,4 @@ namespace Test.QueryTests.Birds
         }
 
     }
-
-    // TestAsyncEnumerator is a simple implementation of IAsyncEnumerator<T> for testing.
-    internal class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
-    {
-        private readonly IEnumerator<T> _enumerator;
-
-        public TestAsyncEnumerator(IEnumerator<T> enumerator)
-        {
-            _enumerator = enumerator ?? throw new ArgumentNullException(nameof(enumerator));
-        }
-
-        public ValueTask<bool> MoveNextAsync()
-        {
-            return new ValueTask<bool>(_enumerator.MoveNext());
-        }
-
-        public T Current => _enumerator.Current;
-
-        public ValueTask DisposeAsync()
-        {
-            _enumerator.Dispose();
-            return new ValueTask();
-        }
-    }
 }
---
+++ Test/TestAsyncEnumerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> _enumerator;
+
+        public TestAsyncEnumerator(IEnumerator<T> enumerator)
+        {
+            _enumerator = enumerator ?? throw new ArgumentNullException(nameof(enumerator));
+        }
+
+        public ValueTask<bool> MoveNextAsync()
+        {
+            return new ValueTask<bool>(_enumerator.MoveNext());
+        }
+
+        public T Current => _enumerator.Current;
+
+        public ValueTask DisposeAsync()
+        {
+            _enumerator.Dispose();
+            return new ValueTask();
+        }
+    }
+}
