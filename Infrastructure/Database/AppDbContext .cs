2a88c1a9ddc7a656e858993c07f7a50211cd143a
musabd96 <113942193+musabd96@users.noreply.github.com>
2023-12-11 13:29:16 +0000

    Bug: fix that delete the bird in Db

--- Infrastructure/Database/AppDbContext .cs
+++ Infrastructure/Database/AppDbContext .cs
@@ -5,17 +5,24 @@ namespace Infrastructure.Database
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext() { }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
         public virtual DbSet<Dog> Dogs { get; set; }
         public virtual DbSet<Cat> Cats { get; set; }
         public virtual DbSet<Bird> Birds { get; set; }
         public virtual DbSet<User> Users { get; set; }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //connectionString to Db
             string connectionString = "Server=localhost;Port=3306;Database=API_Animals;User=root;Password=mustafa0909;";
 
-            optionsBuilder.UseMySql(connectionString, new MariaDbServerVersion(new Version(10, 5, 12)));
+            optionsBuilder.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 35)));
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
 ﻿using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace Infrastructure
 {
@@ -8,6 +10,14 @@ namespace Infrastructure
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
             services.AddSingleton<MockDatabase>();
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                //connectionString to Db
+                string connectionString = "Server=localhost;Port=3306;Database=API_Animals;User=root;Password=mustafa0909;";
+
+                options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 35)));
+            });
+
             return services;
         }
     }
