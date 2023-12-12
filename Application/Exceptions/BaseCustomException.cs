94cb366cc9e97f11115d1c7b0d9d5a6248d51285
musabd96 <mustafa.abdulle@student.nbi-handelsakademin.se>
2023-12-12 21:50:36 +0000

    Feature: add Exceptions in project

---
+++ Application/Exceptions/BaseCustomException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Exceptions
+{
+    public class BaseCustomException : Exception
+    {
+        public BaseCustomException() { }
+
+        public BaseCustomException(string message) : base(message) { }
+
+        public BaseCustomException(string message, Exception inner) : base(message, inner) { }
+    }
+}
